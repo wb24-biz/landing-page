48406852630da46bab86f64181ff19d41395d70b
Vladyslav <vladprodan6@gmail.com>
2025-06-15 14:38:23 +0200

    feat: add focus-visible ring effect to textarea component

--- src/shared/ui/kit/textarea.tsx
+++ src/shared/ui/kit/textarea.tsx
@@ -9,7 +9,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
       className={cn(
         "border-input placeholder:text-muted-foreground",
         "focus:border-[#136EFF] [&:not(:placeholder-shown)]:border-[#136EFF]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
       {...props}
